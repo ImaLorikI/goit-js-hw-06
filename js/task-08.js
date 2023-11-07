a9bc04272956294683cada252c9482adc9c1d344
ImaLorikI <pasha.bandrivskiy@gmail.com>
2023-11-07 20:42:24 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', FormSubmit);
+function FormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    if (mail === '' || password === '') {
+        return alert('Всі поля повинні бути заповнені!');
+    }
+    const formData = {
+        mail,
+        password,
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+}
 
